b6f3246704061f0166e9473086bf08db6a07c5d8
jdkc4d <josh@joshkelahan.com>
2014-04-08 11:17:17 -0500

    Testing for FileExists

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace InstallCentrifyCertificates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.pb_certs = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Location = new System.Drawing.Point(13, 13);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(56, 13);
+            this.lbl_status.TabIndex = 0;
+            this.lbl_status.Text = "Opening...";
+            // 
+            // pb_certs
+            // 
+            this.pb_certs.Location = new System.Drawing.Point(16, 43);
+            this.pb_certs.Name = "pb_certs";
+            this.pb_certs.Size = new System.Drawing.Size(358, 23);
+            this.pb_certs.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 78);
+            this.Controls.Add(this.pb_certs);
+            this.Controls.Add(this.lbl_status);
+            this.Name = "Form1";
+            this.Text = "PSP Install Centrify Certificates";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.ProgressBar pb_certs;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace InstallCentrifyCertificates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public String CurrentDir;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //The entire program launches at load.
+
+            //1. See if the certificate files are present
+            // Get the current folder
+            try
+            {
+                CurrentDir = Directory.GetCurrentDirectory();
+                //MessageBox.Show(CurrentDir);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an error. Please run this program from your desktop.");
+            }
+
+            //Verify the certificates are in the same folder as this exe
+            Boolean RootCertExist = File.Exists(CurrentDir + "\\CentrifyIwaTrustRoot.cer");
+            Boolean HQProxy = File.Exists(CurrentDir + "\\HQ-CENTRIFY01.cer");
+            Boolean OFAProxy = File.Exists(CurrentDir + "\\OFA-CENTRIFY02.cer");
+
+            MessageBox.Show(RootCertExist.ToString() + HQProxy.ToString() + OFAProxy.ToString());
+
+            //2. See if the certificates are already installed
+
+            //3. Install the certificates
+        }
+    }
+}
